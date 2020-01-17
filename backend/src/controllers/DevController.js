f8a85620624416f3ffdb019956d8840685efbbda
Doug <doug@saisp.br>
2020-01-17 18:53:00 -0300

    drud dos devs funcionando

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,52 @@
+const Dev = require('../models/Dev')
+const axios = require('axios')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+//TODO: index,show,update,delete
+
+module.exports = {
+
+    async index(req,res) {
+
+        const devs = await Dev.find()
+
+        return res.json(devs)
+
+    },
+
+    async store(req,res) {
+
+        const { github_username, techs, lat, lon } = req.body
+
+        const respostaGit = await axios.get(`https://api.github.com/users/${github_username}`)
+
+        let dev = await Dev.findOne({github_username})
+
+        if (dev == null) {
+            return res.json({"msg":"Já existe um dev com esse nome"})
+        }
+
+        const { name = login, avatar_url, bio } = respostaGit.data
+
+        const techsArray = parseStringAsArray(techs)
+
+        const location = {
+            type: 'Point',
+            coordinates: [lon, lat]
+        }
+
+        dev = await Dev.create({
+            github_username,
+            name,
+            avatar_url,
+            bio,
+            techs: techsArray,
+            location
+        })
+
+        return res.json(dev)
+
+    }
+
+}
+
---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,31 @@
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+
+    async index(req,res) {
+
+        const { lat,lon,techs } = req.query
+
+        const techArray = parseStringAsArray(techs)
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techArray
+            },
+            location:{
+                $near: {
+                    $geometry: {
+                        type:'Point',
+                        coordinates: [lon,lat]
+                    },
+                    $maxDistance: 10000
+                }
+            }
+        })
+
+        return res.json(devs)
+
+    }
+
+}

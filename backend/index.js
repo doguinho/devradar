add00224ac8824b1dd91f5ad9fb010ae89034c9a
Douglas Silvério <dougsm@gmail.com>
2020-01-20 05:26:43 -0300

    adicionado cors

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
-
+const cors = require('cors')
 const app = express()
 
 mongoose.connect('mongodb+srv://app:2tifwolAoaLD4ilp@cluster0-ghcty.gcp.mongodb.net/test?retryWrites=true&w=majority',
@@ -12,6 +12,7 @@ mongoose.connect('mongodb+srv://app:2tifwolAoaLD4ilp@cluster0-ghcty.gcp.mongodb.
     useFindAndModify:true 
 })
 
+app.use(cors({origin:'http://localhost:3000'}))
 app.use(express.json())
 app.use(routes)
 app.listen(3333)
